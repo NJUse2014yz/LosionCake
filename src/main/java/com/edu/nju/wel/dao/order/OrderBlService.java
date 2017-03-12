12fb17771f8381068f5a8731ee1f1a76fb0e2f96
congye6 <244053679@qq.com>
2017-03-12 12:25:01 +0800

    order

---
+++ src/main/java/com/edu/nju/wel/dao/order/OrderBlService.java
@@ -0,0 +1,39 @@
+package com.edu.nju.wel.dao.order;
+
+import java.util.List;
+
+import com.edu.nju.wel.model.Order;
+
+/**
+ * @author congye6
+ *
+ */
+public interface OrderBlService {
+
+	/**
+	 * 添加订单
+	 * @param order
+	 */
+	public void addOrder(Order order);
+	
+	/**
+	 * 根据用户id获取订单
+	 * @param userId
+	 * @return
+	 */
+	public List<Order> getOrders(int userId);
+	
+	/**
+	 * 根据订单id获取订单
+	 * @param orderId
+	 * @return
+	 */
+	public Order getOrder(int orderId);
+	
+	/**
+	 * 更新订单
+	 * @param order
+	 */
+	public void updateOrder(Order order);
+	
+}
