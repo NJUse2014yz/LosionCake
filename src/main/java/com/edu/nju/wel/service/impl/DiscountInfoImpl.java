e3da5c4a1307e44f81b7f4f112da4c83bf915783
zq14 <zq14@software.nju.edu.cn>
2017-03-12 14:25:37 +0800

    change for discount
    change for discount

--- src/main/java/com/edu/nju/wel/service/impl/DiscountInfoImpl.java
+++ src/main/java/com/edu/nju/wel/service/impl/DiscountInfoImpl.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import com.edu.nju.wel.model.Discount;
 import com.edu.nju.wel.service.DiscountService;
+import com.edu.nju.wel.service.discount.DiscountInfoService;
 
-public class DiscountServiceImpl implements DiscountService{
+public class DiscountInfoImpl implements DiscountInfoService{
 
 	public List<Discount> getDiscount(int GoodID) throws Exception {
 		// TODO Auto-generated method stub
@@ -15,16 +16,6 @@ public class DiscountServiceImpl implements DiscountService{
 	public List<Discount> getDiscount(String GoodName) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	public void changeDiscount(int GoodID, List<Discount> list) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void changeDiscount(String GoodName, List<Discount> list) throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
